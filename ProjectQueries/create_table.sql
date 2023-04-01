49b238dddc4f201d7993ea3b92c0348ad30cd832
Kyler-C1 <kjclark2@ou.edu>
2023-04-01 18:12:47 -0500

    query to create table Login in SQL, complete in Azure

---
+++ ProjectQueries/create_table.sql
@@ -0,0 +1,13 @@
+/*create table Login(
+Username varchar(8) NOT NULL,
+FirstName varchar(50) Not Null,
+LastName varchar(50) Not Null,
+PassWord varchar(30) Not Null,
+DateActive datetime NOT NULL CONSTRAINT DF_Status_DateCreated DEFAULT (getdate()),
+CONSTRAINT PK_Status PRIMARY KEY CLUSTERED (Username))
+*/
+/*
+insert into Login(Username, Firstname, LastName, PassWord)
+values('clar0126', 'Kyler', 'Clark', 'Hoverboard1456!');
+*/
+select * from login
